fd09bac6d0bd1ae14d07d4fd46e5decf78aa0b5a
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-05-14 21:15:44 +1000

    implement reading dicom in processing notebook

--- utils/dicom.py
+++ utils/dicom.py
@@ -1,5 +1,7 @@
 import os
 import pydicom as dicom
+from utils import img
+
 
 def read(path_dcm):
     return dicom.dcmread(os.path.join(path_dcm))
@@ -9,5 +11,17 @@ def convertToGrayPixelArray(pixel_array):
         return (pixel_array / 4096) * 255
     else :
         return 1 - (pixel_array / 4096) * 255
+    
+class DICOM:
+    def __init__(self, path_dcm, path_save):
+        self.path_dcm = path_dcm
+        self.path_save = path_save
+
+    def process(self):
+        image_data = read(self.path_dcm)
+        self.image = convertToGrayPixelArray(image_data.pixel_array)
+        img.saveImageFile(self.path_save, self.image)
+
+        
 
 
