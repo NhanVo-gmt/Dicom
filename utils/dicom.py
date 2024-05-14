ab154661c5fe16452b7887e21f2b26e9ebd458d1
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-05-14 21:57:45 +1000

    fixing saving image

--- utils/dicom.py
+++ utils/dicom.py
@@ -1,16 +1,20 @@
 import os
 import pydicom as dicom
+import numpy as np
 from utils import img
-
+from PIL import Image
 
 def read(path_dcm):
     return dicom.dcmread(os.path.join(path_dcm))
 
 def convertToGrayPixelArray(pixel_array):
     if pixel_array.mean() < 4096 / 2:
-        return (pixel_array / 4096) * 255
+        return convertToJPGFormat(pixel_array)
     else :
-        return 1 - (pixel_array / 4096) * 255
+        return 1 - convertToJPGFormat(pixel_array)
+    
+def convertToJPGFormat(pixel_array):
+    return (np.maximum(pixel_array,0)/pixel_array.max())*255 # float pixels
     
 class DICOM:
     def __init__(self, path_dcm, path_save):
@@ -18,9 +22,15 @@ class DICOM:
         self.path_save = path_save
 
     def process(self):
-        image_data = read(self.path_dcm)
-        self.image = convertToGrayPixelArray(image_data.pixel_array)
-        img.saveImageFile(self.path_save, self.image)
+        self.dcm = read(self.path_dcm)
+        pixel_array = self.dcm.pixel_array.astype(float) 
+        rescaled_image = convertToGrayPixelArray(pixel_array) # Convert to gray scale and JPG format
+
+        final_image = np.uint8(rescaled_image) # integer pixels
+        final_image = Image.fromarray(final_image)
+        final_image.save(self.path_save)
+
+        self.image = final_image
 
         
 
