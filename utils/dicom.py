c06c84d4a5745f327ec12f7e3bd388c7d8955b5b
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-05-19 19:06:47 +1000

    fixing image path

--- utils/dicom.py
+++ utils/dicom.py
@@ -3,23 +3,25 @@ import pydicom as dicom
 import numpy as np
 from utils import img
 from PIL import Image
+from pathlib import Path
 
 def read(path_dcm):
     return dicom.dcmread(os.path.join(path_dcm))
 
 def convertToGrayPixelArray(pixel_array):
     if pixel_array.mean() < 4096 / 2:
-        return convertToJPGFormat(pixel_array)
+        return convertToImageFormat(pixel_array)
     else :
-        return 1 - convertToJPGFormat(pixel_array)
+        return 1 - convertToImageFormat(pixel_array)
     
-def convertToJPGFormat(pixel_array):
+def convertToImageFormat(pixel_array):
     return (np.maximum(pixel_array,0)/pixel_array.max())*255 # float pixels
     
 class DICOM:
-    def __init__(self, path_dcm, path_save):
+    def __init__(self, path_dcm):
         self.path_dcm = path_dcm
-        self.path_save = path_save
+        self.path_save = os.path.dirname(path_dcm) + "/" + Path(path_dcm).stem + ".png"
+        print(self.path_save)
 
     def process(self):
         self.dcm = read(self.path_dcm)
